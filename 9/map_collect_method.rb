bdf28b0904ff9ee810b7f5d3fd6a50099045577e
jmuldvp <jmul.dvp@gmail.com>
2019-08-29 22:45:20 -0600

    r2d12

---
+++ 9/map_collect_method.rb
@@ -0,0 +1,20 @@
+nums = [1, 2, 3, 4, 5]
+squares = []
+nums.each { |i| squares << i ** 2}
+p squares
+
+
+a = nums.map { |e| e ** 2 }
+p a
+
+puts
+puts
+
+fahr = [105, 73, 40, 18, -2]
+
+cel_temp = fahr.map do |temp|
+  minus32 = temp - 32
+  minus32 * (5.0/9.0)
+end
+
+p cel_temp
